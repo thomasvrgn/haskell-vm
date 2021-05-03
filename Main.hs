5089896d79ee309f2df0ff17af8a675039ce46b1
thomasvergne <thomas@quark-lang.dev>
2021-05-03 22:56:13 +0200

    ➕ Types modules imported into main.

--- Main.hs
+++ Main.hs
@@ -1,27 +1,7 @@
 {-# LANGUAGE BlockArguments #-}
 module Main where
-  import Control.Monad.State
+  import Types.Value
+  import Types.Stack
+  import Types.Bytecode
 
-  data Value
-    = String String
-    | Integer Integer
-    deriving (Show)
-
-  type Frame = [Value]
-
-  type Address = Int
-  type Pointer = (String, Address)
-  type Symbols = [Pointer]
-
-  type Stack = [Frame]
-  type Memory = (Symbols, Stack)
-
-  data Instruction
-    = PUSH Value
-    | POP
-    | STORE String
-
-  type Page = [Instruction]
-
-  main :: IO ()
-  main = print (String "test")
+  main = print "test"
