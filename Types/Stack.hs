0e2f233f527f202163883964b6cf5664d2b710ca
thomasvergne <thomas@quark-lang.dev>
2021-05-03 22:56:01 +0200

    ✨ Stack function utils added.

--- Types/Stack.hs
+++ Types/Stack.hs
@@ -9,3 +9,22 @@ module Types.Stack where
 
   type Memory = (Stack, Symbols)
   
+  -- Stack methods
+  push :: Value -> Stack -> Stack
+  push value stack = stack ++ [value]
+
+  getLast :: Stack -> Value
+  getLast = last
+
+  removeLast :: Stack -> Stack
+  removeLast = init
+
+  pop :: Stack -> (Value, Stack)
+  pop = (,) <$> getLast <*> removeLast
+
+  -- Symbols methods
+  register :: Symbol -> Symbols -> Symbols
+  register (name, address) symbols = symbols ++ [(name, address)]
+
+  remove :: String -> Symbols -> Symbols
+  remove name = filter ((/=name) . fst)
