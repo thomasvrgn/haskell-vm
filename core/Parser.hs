a3da65400022367f9e931cf5d49f36011ffde2e4
thomasvergne <thomas@quark-lang.dev>
2021-05-04 13:15:14 +0200

    ✨ Bytecode parser added.

--- core/Parser.hs
+++ core/Parser.hs
@@ -3,6 +3,12 @@ module Core.Parser where
   import Types.Stack
   import Types.Value
   import Data.Char
+  import Data.Typeable
+  import Data.Maybe
+  import Data.Char
+
+  maybeRead2 :: Read a => String -> Maybe a
+  maybeRead2 = fmap fst . listToMaybe . filter (null . dropWhile isSpace . snd) . reads
 
   isAllUpper :: String -> Bool
   isAllUpper = all isUpper
@@ -12,11 +18,30 @@ module Core.Parser where
 
   type Expression = [String]
 
-  parse :: String -> [Expression]
-  parse content = parse' (words content) ([], [[]])
+  regroup :: String -> [Expression]
+  regroup content = parse' (words content) ([], [[]])
     where parse' :: [String] -> ([String], [Expression]) -> [[String]]
           parse' (x:xs) (state, ast) =
             if (isAllUpper x) 
               then parse' xs ([x], ast ++ [state])
               else parse' xs (state ++ [x], ast)
           parse' [] (state, ast) = removeEmptyLists $ ast ++ [state]
+  
+
+  valueify :: String -> Value
+  valueify elem = case (maybeRead2 elem) of
+    (Just a) -> Integer a
+    Nothing -> String ((drop 1) . init $ elem)
+
+  process :: [Expression] -> Bytecode
+  process [] = []
+  process (x:xs) = 
+    (let (expr:args) = x in case expr of
+      "PUSH" -> let (elem:_) = args in [Push (valueify elem)]
+      "STORE" -> let (name:_) = args in [Store name]
+      "LOAD" -> let (name:_) = args in [Load name]
+      "DROP" -> let (name:_) = args in [Drop name]
+      ) ++ process xs
+
+  parse :: String -> Bytecode
+  parse = process . regroup
