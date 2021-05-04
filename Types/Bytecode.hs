1c5a32cd65f823b9072be3246db477ea6dbfddef
thomasvergne <thomas@quark-lang.dev>
2021-05-04 13:32:51 +0200

    ✨ Bytecode printing method added.

--- Types/Bytecode.hs
+++ Types/Bytecode.hs
@@ -17,5 +17,22 @@ module Types.Bytecode where
     | Mul
     | Div
     | Sub
+    deriving Show
 
   type Bytecode = [Instruction]
+
+  -- Bytecode methods
+  getValue :: Value -> String
+  getValue (String t) = t
+  getValue (Integer t) = show t
+
+  printBytecode :: Bytecode -> IO()
+  printBytecode [] = return ()
+  printBytecode (x:xs) = do
+    case x of 
+      Push a -> putStr $ "PUSH " ++ getValue a
+      Store a -> putStr $ "STORE " ++ a
+      Load a -> putStr $ "LOAD " ++ a
+      Drop a -> putStr $ "DROP " ++ a
+    putStrLn []
+    printBytecode xs
