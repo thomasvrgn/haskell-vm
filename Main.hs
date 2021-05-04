d13505acfa3e1ea3d2a1de3747f0f0ff3c6668c9
thomasvergne <thomas@quark-lang.dev>
2021-05-04 11:56:19 +0200

    ✨ Basic Store, Load and Push VM added.

--- Main.hs
+++ Main.hs
@@ -5,15 +5,27 @@ module Main where
   import Types.Bytecode
   import Control.Monad.State
   
-  stackTests = do
-    (stack, symbols) <- get
-    push (Integer 7)
-    pop
-    push (String "test")
-    register ("test", 15)
-    remove "test"
+  runBytecode :: Bytecode -> State Memory ()
+  runBytecode [] = return ()
+  runBytecode (x:xs) = evalInstruction x
+    where 
+      evalInstruction (Push value) = do
+        push value
+        runBytecode xs
+
+      evalInstruction (Store name) = do
+        index <- stackLength
+        register (name, index)
+        runBytecode xs
+      
+      evalInstruction (Load name) = do
+        (_, index) <- getVariable name
+        (stack, _) <- get
+        push (stack !! index)
+        runBytecode xs
 
   main :: IO()
   main = do
-    let res = runState (stackTests) emptyMemory
-    print res
+    let (ret, st) = runState (runBytecode [Push (String "test"), Store "a", Load "a", Load "a"]) emptyMemory
+    printMem st
+    print "test"
