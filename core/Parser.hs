4fc5f470b948453cb8163df5b1ee9061f846ec86
thomasvergne <thomas@quark-lang.dev>
2021-05-10 22:35:12 +0200

    ✨ Instruction parsing added.

--- core/Parser.hs
+++ core/Parser.hs
@@ -6,12 +6,31 @@ module Core.Parser where
   import Data.Char
   import Data.Typeable
   import Data.Maybe
-  import Data.Char
+  import Data.List
 
   endWiths :: Char -> String -> Bool
   endWiths x = (==x) . last
 
+  joinSpace :: [String] -> String
+  joinSpace = concat . intersperse " "
+
+  isAllUpper :: String -> Bool
+  isAllUpper = all isUpper
+
+  removeEmptyLists :: [[a]] -> [[a]]
+  removeEmptyLists = filter ((/=0) . length)
+
   type Segments = [(String, [String])]
+  type Expression = [String]
+
+  regroup :: [String] -> [Expression]
+  regroup content = parse' content ([], [[]])
+    where parse' :: [String] -> ([String], [Expression]) -> [[String]]
+          parse' (x:xs) (state, ast) =
+            if isAllUpper x
+              then parse' xs ([x], ast ++ [state])
+              else parse' xs (state ++ [x], ast)
+          parse' [] (state, ast) = removeEmptyLists $ ast ++ [state]
 
   parse :: String -> Segments
   parse x = parse' split ([], [])
@@ -20,6 +39,6 @@ module Core.Parser where
           parse' [] (content, segments) = let (seg:instr) = content in segments ++ [(seg, instr)]
           parse' (x:xs) (el, seg) = if endWiths ':' x
             then if length el > 0 
-              then let (seg':content) = el in parse' xs ([x], seg ++ [(seg', content)])
-              else parse' xs ([x], seg)
+              then let (seg':content) = el in parse' xs ([init x], seg ++ [(seg', content)])
+              else parse' xs ([init x], seg)
             else parse' xs (el ++ [x], seg)
