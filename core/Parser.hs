4692c8793a7f6561b3f50602aba8efb0a8fca2c2
thomasvergne <thomas@quark-lang.dev>
2021-05-11 19:46:10 +0200

    🐛 Words replaced with clear lexer.

--- core/Parser.hs
+++ core/Parser.hs
@@ -3,6 +3,8 @@ module Core.Parser where
   import Types.Stack
   import Types.Value
 
+  import Core.Lexer
+
   import Data.Char
   import Data.Typeable
   import Data.Maybe
@@ -32,13 +34,47 @@ module Core.Parser where
               else parse' xs (state ++ [x], ast)
           parse' [] (state, ast) = removeEmptyLists $ ast ++ [state]
 
-  parse :: String -> Segments
-  parse x = parse' split ([], [])
-    where split  = words x
-          parse' :: [String] -> ([String], Segments) -> Segments
+  process :: [String] -> Segments
+  process x = parse' x ([], [])
+    where parse' :: [String] -> ([String], Segments) -> Segments
           parse' [] (content, segments) = let (seg:instr) = content in segments ++ [(seg, instr)]
           parse' (x:xs) (el, seg) = if endWiths ':' x
             then if length el > 0 
               then let (seg':content) = el in parse' xs ([init x], seg ++ [(seg', content)])
               else parse' xs ([init x], seg)
             else parse' xs (el ++ [x], seg)
+
+  readMaybe :: (Read a) => String -> Maybe a
+  readMaybe s = case reads s of
+                  [(x, "")] -> Just x
+                  _ -> Nothing
+
+  valueify :: String -> Value
+  valueify elem = case (readMaybe elem) of
+    (Just a) -> Integer a
+    Nothing -> String ((drop 1) . init $ elem)
+
+  transform :: [Expression] -> Bytecode
+  transform [] = []
+  transform (x:xs) = 
+    (let (expr:args) = x in case uppercase expr of
+      "LOAD" -> let (name:_) = args in [Load name]
+      "LOAD_SECTION" -> let (section:_) = args in [LoadSection section]
+
+      "PUSH" -> let (elem:_) = args in [Push (valueify elem)]
+      "POP" -> [Pop]
+
+      "STORE" -> let (name:_) = args in [Store name]
+      "DROP" -> let (name:_) = args in [Drop name]
+      "CALL" -> let (argsLength:_) = args in [Call (read argsLength :: Int)]
+
+      "ADD" -> [Add]
+      "MUL" -> [Mul]
+      "SUB" -> [Sub]
+      "DIV" -> [Div]
+
+      _ -> error ("Can't recognize instruction " ++ expr ++ "!")
+      ) ++ transform xs
+
+  parse :: String -> [(String, Bytecode)]
+  parse = map (fmap (transform . regroup)) . process . tokenize
